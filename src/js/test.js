2c27fb302ccbc2dc6fbc8266061f2b077365449c
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-11-01 14:12:08 +0100

    la till eventListener på input

--- src/js/test.js
+++ src/js/test.js
@@ -7,14 +7,27 @@ getContainer.appendChild(ul);
 let myList = ["Köp potatis", "Ta ut hunden", "Betala räkningar"];
 
 for(let i = 0; i < myList.length; i++) {
+    //Skapar <li>
     let li = document.createElement("li");
     ul.appendChild(li);
 
+    //Publicerar listan i min <li>
     li.innerHTML = myList[i];
 
+    //Skapar <input> 
     let inputBox = document.createElement("input");
     li.appendChild(inputBox);
     inputBox.setAttribute("type", "checkbox");
 
-    
+    //Skapar händelse 
+    inputBox.addEventListener("click", markAsChecked);
+
+    //Kollar om vi checkat vår inputBox
+    function markAsChecked() {
+        if(inputBox.checked === true) {
+            li.innerHTML = "Färdig med uppgiften";
+        }
+    }
+
+
 }
