734a3574724f69ec4fb4a9819690477c95dc0f75
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-11-03 12:17:20 +0100

    ordnade med splice

--- src/js/test.js
+++ src/js/test.js
@@ -19,80 +19,105 @@ let myList = [
   new listObjects("Ta ut hunden", false),
 ];
 
-for (let i = 0; i < myList.length; i++) {
-  //Skapar <li>
-  let li = document.createElement("li");
-  ul.appendChild(li);
 
-  let pTag = document.createElement("p");
-  li.appendChild(pTag);
 
-  //Skapar knapp
-  let deleteBtn = document.createElement("button");
-  deleteBtn.innerHTML = "X";
+function existingList() {
+  for (let i = 0; i < myList.length; i++) {
+    //Skapar <li>
+    let li = document.createElement("li");
 
-  let buttonFunction = deleteBtn.addEventListener("click", () => {
-    console.log("Klickad");
+    //Tömma ul innan 
+    ul.appendChild(li);
 
-    //Tar bort värdet från listan med splice
-    let deleteArray = myList.splice(i, 1);
-    console.log(deleteArray);
+    //Lägger p tag i <li>
+    let pTag = document.createElement("p");
+    li.appendChild(pTag);
 
-    // if ((deleteArray = true)) {
-    //   li.remove();
-    // }
-  });
+    // Publicerar lista med objekt i min <li>
+    pTag.innerHTML = myList[i].toDo;
 
-  // Publicerar lista med objekt i min <li>
-  pTag.innerHTML = myList[i].toDo;
+    let clickedItem = myList[i];
 
-  //Skapar <input>
-  let inputBox = document.createElement("input");
-  li.appendChild(inputBox);
-  inputBox.classList.add("box");
-  inputBox.setAttribute("type", "checkbox");
+    //Skapar <input>
+    let inputBox = document.createElement("input");
+    li.appendChild(inputBox);
+    inputBox.classList.add("box");
+    inputBox.setAttribute("type", "checkbox");
 
-  //Skapar händelse
-  inputBox.addEventListener("click", () => {
-    if (inputBox.checked === true) {
-      myList[i].done = true;
-      pTag.innerHTML = "Uppgift avklarad";
-      li.appendChild(deleteBtn);
-    } else {
-      myList[i].done = false;
-      pTag.innerHTML = myList[i].toDo;
-      deleteBtn.remove();
-    }
-  });
+    inputBox.addEventListener("click", () => {
+      handleClick(li, myList[i], inputBox, ul);
+    });
+  }
 }
 
+function handleClick(li, clickedItem, inputBox) {
+  if (inputBox.checked === true) {
+    clickedItem.done = true;
+    console.log(true);
+
+    let index = myList.indexOf(clickedItem);
+    myList.splice(index, 1);
+
+    console.log("Borttagen " + index);
+    console.log(myList);
+
+  }
+  ul.innerHTML = " ";
+  existingList()
+}
+
+existingList();
+
+//
+
+let addToList = [];
+
+//Hittar input fältet
 let sendValue = document.getElementById("inputButton");
 let typeInput = document.getElementById("inputField");
 
 sendValue.addEventListener("click", newToDo);
-
-let addToList = [];
 
 function newToDo() {
   addToList.push(typeInput.value);
   console.log(addToList);
 
   let newLi = document.createElement("li");
-    ul.appendChild(newLi);
+  ul.appendChild(newLi);
 
-    newLi.innerHTML += addToList;
-    addToList.pop();
+  newLi.innerHTML += addToList;
+  addToList.pop();
 
-    let inputBox = document.createElement("input");
-    newLi.appendChild(inputBox);
+  let inputBox = document.createElement("input");
+  newLi.appendChild(inputBox);
 
-    // inputBox.classList.add("box");
-    inputBox.style.width = "40px";
-    inputBox.setAttribute("type", "checkbox");
+  let deleteNewValue = document.createElement("button");
+  deleteNewValue.inputBox.classList.add("box");
+  inputBox.style.width = "40px";
+  inputBox.setAttribute("type", "checkbox");
+
+  inputBox.addEventListener("click", () => {
+    if (inputBox.checked === true) {
+      newLi.innerHTML = "Uppgiften avklarad";
+    }
+  });
 
   clearInput();
 }
 
 function clearInput() {
   typeInput.value = " ";
+}
+
+function button() {
+  //Skapar knapp
+  // let deleteBtn = document.createElement("button");
+  // deleteBtn.innerHTML = "X";
+  //Skapar event listener för ta bort knappen
+  // let buttonFunction = deleteBtn.addEventListener("click", () => {
+  //   console.log("Klickad");
+  //   //Tar bort värdet från listan med splice
+  //   let deleteArray = myList.splice(i, 1);
+  //   console.log(deleteArray);
+  // });
 }
