683f6980d546713181bcbbf58741f0c251e0f6a6
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-11-04 10:22:55 +0100

    la till en if sats för listan

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,15 @@ import { listObjects } from "./models/todo";
 let getContainer = document.getElementById("container");
 let newContainer = document.getElementById("finished");
 
-//Skapar ul
+//Ul för lista som inte är klar
 let ul = document.createElement("ul");
 getContainer.appendChild(ul);
 
-let regretBtn = document.getElementById("regret");
-
+//Ul för lista som är klar
 let newUl = document.createElement("ul");
 newContainer.appendChild(newUl);
+
+let regretBtn = document.getElementById("regret");
 
 let myList = [
   new listObjects("Köp Pepsi Max", false),
@@ -20,6 +21,7 @@ let myList = [
 
 function existingList() {
   for (let i = 0; i < myList.length; i++) {
+
     //Skapar <li>
     let li = document.createElement("li");
 
@@ -29,8 +31,9 @@ function existingList() {
     let pTag = document.createElement("p");
     li.appendChild(pTag);
 
-    // Publicerar lista med objekt i min <li>
-    pTag.innerHTML = myList[i].toDo;
+    if(myList[i].done === false) {
+      pTag.innerHTML = myList[i].toDo;
+    }
 
     let inputBox = document.createElement("input");
     li.appendChild(inputBox);
@@ -38,28 +41,16 @@ function existingList() {
     inputBox.setAttribute("type", "checkbox");
 
       inputBox.addEventListener("click", () => {
-        handleClick(myList[i], inputBox);
+        handleClick(myList[i], inputBox, pTag);
       });
     };
   }
-
-  //Skapar <input>
-  //   let inputBox = document.createElement("input");
-  //   li.appendChild(inputBox);
-  //   inputBox.classList.add("box");
-  //   inputBox.setAttribute("type", "checkbox");
-
-  //   inputBox.addEventListener("click", () => {
-  //     handleClick(myList[i], inputBox);
-  //   });
-  // }
-
 
 //Ny lista där avklarade objekt läggs till i
 let finishedList = [];
 
 //När man kryssar i input körs denna funktion
-function handleClick(clickedItem, inputBox) {
+function handleClick(clickedItem, inputBox, pTag) {
   if (inputBox.checked === true) {
     clickedItem.done = true;
 
@@ -70,34 +61,26 @@ function handleClick(clickedItem, inputBox) {
     console.log("Uppgiften avklarad");
     console.log(myList);
   }
+
   ul.innerHTML = " ";
   existingList();
 
-  //Flyttar de avklarade objekten till ny lista
-  if ((clickedItem.done = true)) {
-    let li = document.createElement("li");
-    newUl.appendChild(li);
+  //Efter användaren kryssat i inputboxen hamnar de avklarade to do objekten här
+  let newLi = document.createElement("li");
+  newUl.appendChild(newLi);
 
-    let undoButton = document.getElementById("remove");
+  let newPTag = document.createElement("p");
+  newLi.appendChild(newPTag);
 
-    //Pushar de klara uppgifterna till nya listan
-    finishedList.push(clickedItem.toDo);
+  newLi.appendChild(inputBox);
 
-    li.innerHTML = finishedList;
+  if(clickedItem.done === true) {
+    newPTag.innerHTML = clickedItem.toDo;
 
-    //Ser till att det inte blir dubblering i HTML från listan
-    finishedList.pop();
-
-    regretBtn.addEventListener("click", () => {
-      console.log("Du ångrade dig!");
-      myList.push(finishedList);
-
-      li.innerHTML = clickedItem.toDo;
-      console.log(clickedItem.toDo);
-      ul.appendChild(li);
-    });
+   
   }
-  newToDo(inputBox);
+
+  
 }
 
 let inputValue = document.getElementById("inputNewValue");
